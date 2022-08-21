2fca60515ddd60d5e37931db20fbb9de1bb7aabe
Talman95 <taltro528@gmail.com>
2022-08-21 22:00:20 +0300

    Prepared a project

--- src/c1-main/m2-bll/store.ts
+++ src/c1-main/m2-bll/store.ts
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers} from "redux";
 import {loginReducer} from "../../c2-features/f1-auth/a1-login/l2-bll/loginReducer";
 import {registerReducer} from "../../c2-features/f1-auth/a2-register/r2-bll/registerReducer";
 import {profileReducer} from "../../c2-features/f1-auth/a3-profile/p2-bll/profileReducer";
 import {forgotReducer} from "../../c2-features/f1-auth/a4-forgot/f2-bll/forgotReducer";
 import {setPassReducer} from "../../c2-features/f1-auth/a5-setPass/s2-bll/setPassReducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 const rootReducers = combineReducers({
     login: loginReducer,
@@ -13,7 +14,9 @@ const rootReducers = combineReducers({
     setPass: setPassReducer,
 })
 
-export const store = createStore(rootReducers)
+export const store = configureStore({
+    reducer: rootReducers
+})
 
 export type RootStateType = ReturnType<typeof rootReducers>
 
